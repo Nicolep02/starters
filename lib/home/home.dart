86bd7fc7361564a259004fced3ece0973ec5fba9
Nicolep02 <nicolewphan02@gmail.com>
2023-07-18 14:43:08 -0700

    Internationalization/Localization start
    added l10n.yaml and arb files. Updated converMain, greetings, languageSelection dart files and pubspec.yaml for localization

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -10,140 +10,142 @@ class Home extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Color.fromRGBO(79, 148, 156, 1.0),
-      body: Align(
-        child: Container(
-          height: 1200,
-          width: 600,
-          decoration: new BoxDecoration(
-            color: Color.fromRGBO(21, 33, 61, 1.0),
-            borderRadius: new BorderRadius.all(Radius.elliptical(120, 150)),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child:Text('Hello,\n'
-                    'Starters!',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                    color: Colors.white,
-                    fontFamily: 'IrishGrover',
+      body: SingleChildScrollView(
+        child: Align(
+          child: Container(
+            height: 1200,
+            width: 600,
+            decoration: new BoxDecoration(
+              color: Color.fromRGBO(21, 33, 61, 1.0),
+              borderRadius: new BorderRadius.all(Radius.elliptical(120, 150)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child:Text('Hello,\n'
+                      'Starters!',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                      color: Colors.white,
+                      fontFamily: 'IrishGrover',
+                    ),
+                  ),
+                  padding: EdgeInsets.fromLTRB(0, 150, 90, 5),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(170, 0, 0, 0),
+                  child: Image.asset(
+                    'assets/homeBunni.png',
+                    width: 167.97,
+                    height: 157.6,
                   ),
                 ),
-                padding: EdgeInsets.fromLTRB(0, 150, 90, 5),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(170, 0, 0, 0),
-                child: Image.asset(
-                  'assets/homeBunni.png',
-                  width: 167.97,
-                  height: 157.6,
-                  ),
-                ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(0, 15, 4, 0),
-                        child: Image.asset(
-                          "assets/pointer.png",
-                          width: 35, height: 33,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 15, 4, 0),
+                          child: Image.asset(
+                            "assets/pointer.png",
+                            width: 35, height: 33,
+                          ),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(0, 50, 0, 30),
-                        child: TextButton(
-                          onPressed: (){
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Language()));
-                          },
-                          // onHover: ,
-                          // style: ButtonStyle(
-                          //
-                          // ),
-                          child: Text('Let\'s Learn ESL',
-                            style: TextStyle(
-                              fontFamily: 'IrishGrover',
-                              color: Colors.white,
-                              fontSize: 24,
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 50, 0, 30),
+                          child: TextButton(
+                            onPressed: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Language()));
+                            },
+                            // onHover: ,
+                            // style: ButtonStyle(
+                            //
+                            // ),
+                            child: Text('Let\'s Learn ESL',
+                              style: TextStyle(
+                                fontFamily: 'IrishGrover',
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
                             ),
                           ),
                         ),
-                      ),
 
-                    ],
-                  ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 0, 4, 30),
-                    child: Image.asset(
-                      "assets/pointer.png",
-                      width: 35, height: 33,
+                      ],
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                    child: TextButton(
-                      onPressed: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Help()));
-                      },
-                      // onHover: ,
-                      // style: ButtonStyle(
-                      //
-                      // ),
-                      child: Text('Help',
-                        style: TextStyle(
-                          fontFamily: 'IrishGrover',
-                          color: Colors.white,
-                          fontSize: 24,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 0, 4, 30),
+                          child: Image.asset(
+                            "assets/pointer.png",
+                            width: 35, height: 33,
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 0, 4, 0),
-                    child: Image.asset(
-                      "assets/pointer.png",
-                      width: 35, height: 33,
-                    ),
-                  ),
-                  Container(
-                    child: TextButton(
-                      onPressed: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Resources()));
-                      },
-                      // onHover: ,
-                      // style: ButtonStyle(
-                      //
-                      // ),
-                      child: Text('Resources',
-                        style: TextStyle(
-                          fontFamily: 'IrishGrover',
-                          color: Colors.white,
-                          fontSize: 24,
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                          child: TextButton(
+                            onPressed: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Help()));
+                            },
+                            // onHover: ,
+                            // style: ButtonStyle(
+                            //
+                            // ),
+                            child: Text('Help',
+                              style: TextStyle(
+                                fontFamily: 'IrishGrover',
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-                ],
-              ),
-            ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 0, 4, 0),
+                          child: Image.asset(
+                            "assets/pointer.png",
+                            width: 35, height: 33,
+                          ),
+                        ),
+                        Container(
+                          child: TextButton(
+                            onPressed: (){
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Resources()));
+                            },
+                            // onHover: ,
+                            // style: ButtonStyle(
+                            //
+                            // ),
+                            child: Text('Resources',
+                              style: TextStyle(
+                                fontFamily: 'IrishGrover',
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,56 @@
 // import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import '../home/home.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() => runApp(const MaterialApp(
-    home: Home(),
-    initialRoute: '/',
-    // routes:{
-    //     // '/':(context) => Loading(),
-    //     '/home': (context) => const Home(),
-    //     '/resources': (context) => Resources(),
-    // },
-));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+  static _MyAppState? of(BuildContext context) => context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+    Locale _locale = const Locale('en');
+
+    void setLocale(Locale value) {
+        setState(() {
+            _locale = value;
+        });
+    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        localizationsDelegates: const [
+            AppLocalizations.delegate, // Add this line
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+        ],
+
+        supportedLocales: const [
+            Locale('en'), // English
+            Locale('ja'), // Japanese
+            Locale('ko'), // Korean
+            Locale('zh'), // Chinese
+            Locale('es'), // Spanish
+            Locale('vi'), // Vietnamese
+            Locale('fr'), // French
+            Locale('it'), // Italian
+            Locale('de'), // German
+            Locale('pt'), // Portuguese
+        ],
+
+        locale: _locale,
+        home: const Home(),
+    );
+  }
+}
 
 
 
