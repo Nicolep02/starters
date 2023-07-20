9e3624ca8531d299cc89a0d9b945b3fc86660922
Nicolep02 <nicolewphan02@gmail.com>
2023-07-19 23:13:50 -0700

    edited assets and language files

--- lib/styles/spacing.dart
+++ lib/styles/spacing.dart
@@ -1,10 +1,10 @@
 //general
 double appbar_height = 103;
-double title_subtitle_spacing = 50;
-double bottom_padding = 50;
+double title_subtitle_spacing = 60;
+double bottom_padding = 30;
 
 //choose language page
-double subTitle_flag_spacing = 40;
+double subTitle_flag_spacing = 50;
 double flag_width = 90;
 double flag_height = 80;
 double flag_bottom_spacing = 40;
