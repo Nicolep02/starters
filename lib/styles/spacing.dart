927d237d105fa37115abc51a4e784c11f5f7d56b
Nicolep02 <nicolewphan02@gmail.com>
2023-07-25 15:43:10 -0700

    Updated UI and fixed main page's template

--- lib/styles/spacing.dart
+++ lib/styles/spacing.dart
@@ -1,6 +1,7 @@
 //general
 double appbar_height = 103;
-double title_subtitle_spacing = 60;
+double title_subtitle_spacing = 40;
+double subtitles = 15;
 double bottom_padding = 30;
 
 //choose language page
@@ -14,11 +15,15 @@ double topic_width = 225;
 double topic_height = 225;
 
 
-//converMain page
-double container_width = 331;
-double container_height = 68;
-double icon_dimen = 68;
-double icon_text = 5;
+//TopicMain page
+double subtitle_container = 40;
+double container_width = 320;
+double container_height = 80;
+double container_spacings = 20;
+double icon_dimen = 110;
+double text_icon = 15;
+double text = 8;
+double text_wrap_width = 162;
 double text_arrow = 90;
 double categories = 30;
 
